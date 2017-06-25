63af01594bf93571bb096be9eeb6bfcf3b1e6718
Kamil Danak <kamildanak@gmail.com>
2017-06-25 17:04:40 +0200

    Rename to Cornerstone

--- src/main/java/com/kamildanak/minecraft/cornerstone/Cornerstone.java
+++ src/main/java/com/kamildanak/minecraft/cornerstone/Cornerstone.java
@@ -1,9 +1,9 @@
-package com.kamildanak.minecraft.realestate;
+package com.kamildanak.minecraft.cornerstone;
 
 import com.kamildanak.minecraft.enderpay.events.EventHandler;
-import com.kamildanak.minecraft.realestate.blocks.BlockCornerstone;
-import com.kamildanak.minecraft.realestate.data.ChunkData;
-import com.kamildanak.minecraft.realestate.settings.Settings;
+import com.kamildanak.minecraft.cornerstone.blocks.BlockCornerstone;
+import com.kamildanak.minecraft.cornerstone.data.ChunkData;
+import com.kamildanak.minecraft.cornerstone.settings.Settings;
 import net.minecraft.block.Block;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.World;
@@ -16,20 +16,20 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 import javax.annotation.Nonnull;
 
-import static com.kamildanak.minecraft.realestate.Utils.getWorldDir;
+import static com.kamildanak.minecraft.cornerstone.Utils.getWorldDir;
 
-@Mod(modid = RealEstate.modID, name = RealEstate.modName, version = RealEstate.version,
-        acceptedMinecraftVersions = RealEstate.mcVersion)
-public class RealEstate {
-    public static final String saveLocation = "RealEstate-data";
-    static final String modID = "realestate";
+@Mod(modid = Cornerstone.modID, name = Cornerstone.modName, version = Cornerstone.version,
+        acceptedMinecraftVersions = Cornerstone.mcVersion)
+public class Cornerstone {
+    public static final String saveLocation = "Cornerstone-data";
+    static final String modID = "cornerstone";
     static final String modName = "{$modName}";
     static final String version = "{$modVersion}";
     static final String mcVersion = "{@mcVersion}";
     public static Settings settings;
     @Mod.Instance(modID)
     @SuppressWarnings("unused")
-    public static RealEstate instance;
+    public static Cornerstone instance;
     private static Block blockCornerstone;
 
     @Mod.EventHandler
