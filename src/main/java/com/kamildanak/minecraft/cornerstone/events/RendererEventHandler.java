5a98c87ee9755f789d593e8ce4dc159c5c3ded9c
Kamil Danak <kamildanak@gmail.com>
2017-07-03 12:11:39 +0200

    Draw line between connected cornerstonesUnderConstruction

--- src/main/java/com/kamildanak/minecraft/cornerstone/events/RendererEventHandler.java
+++ src/main/java/com/kamildanak/minecraft/cornerstone/events/RendererEventHandler.java
@@ -12,14 +12,17 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.ArrayList;
+
 public class RendererEventHandler {
-    Minecraft mc;
+    private Minecraft mc;
 
     public RendererEventHandler(Minecraft mc) {
         this.mc = mc;
     }
 
-    public static void render(double partialTicks, EntityPlayer entityplayer) {
+    private static void render(double partialTicks, EntityPlayer entityplayer) {
+        ArrayList<BlockPos> cornerstones = PlayerData.get(entityplayer.getUniqueID()).getCornerstoneUnderConstruction();
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferbuilder = tessellator.getBuffer();
         GlStateManager.disableTexture2D();
@@ -34,14 +37,61 @@ public class RendererEventHandler {
         bufferbuilder.begin(3, DefaultVertexFormats.POSITION_COLOR);
 
         int maxLen = Cornerstone.settings.getMaxPlotLength();
-        for (BlockPos pos : PlayerData.get(entityplayer.getUniqueID()).getCornerstoneUnderConstruction()) {
+        ArrayList<Double> pointsX = new ArrayList<>();
+        ArrayList<Double> pointsZ = new ArrayList<>();
+        for (BlockPos pos : cornerstones) {
             double x = pos.getX();
             double y = pos.getY();
             double z = pos.getZ();
-            addLine(bufferbuilder, x + 0.5, y + 0.5, z + 0.5, x - 0.5 + maxLen, y + 0.5, z + 0.5, dx, dy, dz);
-            addLine(bufferbuilder, x + 0.5, y + 0.5, z + 0.5, x + 1.5 - maxLen, y + 0.5, z + 0.5, dx, dy, dz);
-            addLine(bufferbuilder, x + 0.5, y + 0.5, z + 0.5, x + 0.5, y + 0.5, z - 0.5 + maxLen, dx, dy, dz);
-            addLine(bufferbuilder, x + 0.5, y + 0.5, z + 0.5, x + 0.5, y + 0.5, z + 1.5 - maxLen, dx, dy, dz);
+            boolean drawX = true;
+            boolean drawZ = true;
+            for (BlockPos pos2 : cornerstones) {
+                if (pos == pos2) continue;
+                if (pos.getX() == pos2.getX()) {
+                    drawZ = false;
+                    addLine(bufferbuilder, x + 0.5, y + 0.5, z + 0.5,
+                            x + 0.5, y + 0.5, pos2.getZ() + 0.5, dx, dy, dz);
+                }
+                if (pos.getZ() == pos2.getZ()) {
+                    drawX = false;
+                    addLine(bufferbuilder, x + 0.5, y + 0.5, z + 0.5,
+                            pos2.getX() + 0.5, y + 0.5, z + 0.5, dx, dy, dz);
+                }
+            }
+            if (cornerstones.size() >= 3) {
+                if (pointsX.contains(x)) {
+                    pointsX.remove(x);
+                } else {
+                    pointsX.add(x);
+                }
+                if (pointsZ.contains(z)) {
+                    pointsZ.remove(z);
+                } else {
+                    pointsZ.add(z);
+                }
+                continue;
+            }
+            if (drawX)
+                addLine(bufferbuilder, x + 1.5 - maxLen, y + 0.5, z + 0.5,
+                        x - 0.5 + maxLen, y + 0.5, z + 0.5, dx, dy, dz);
+            if (drawZ)
+                addLine(bufferbuilder, x + 0.5, y + 0.5, z + 1.5 - maxLen,
+                        x + 0.5, y + 0.5, z - 0.5 + maxLen, dx, dy, dz);
+        }
+        if (cornerstones.size() >= 3 && pointsX.size() == 1 && pointsZ.size() == 1) {
+            for (BlockPos pos : cornerstones) {
+                double x = pos.getX();
+                double y = pos.getY();
+                double z = pos.getZ();
+                if (pos.getX() == pointsX.get(0)) {
+                    addLine(bufferbuilder, x + 0.5, y + 0.5, z + 0.5,
+                            x + 0.5, y + 0.5, pointsZ.get(0) + 0.5, dx, dy, dz);
+                }
+                if (pos.getZ() == pointsZ.get(0)) {
+                    addLine(bufferbuilder, x + 0.5, y + 0.5, z + 0.5,
+                            pointsX.get(0) + 0.5, y + 0.5, z + 0.5, dx, dy, dz);
+                }
+            }
         }
 
         tessellator.draw();
