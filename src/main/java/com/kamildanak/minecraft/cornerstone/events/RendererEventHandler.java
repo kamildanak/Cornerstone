04a098bb3497048dd7d9fc83e8d55b9820bed538
Kamil Danak <kamildanak@gmail.com>
2017-07-03 13:22:16 +0200

    Draw 4 lines instead of one between BlockPos

--- src/main/java/com/kamildanak/minecraft/cornerstone/events/RendererEventHandler.java
+++ src/main/java/com/kamildanak/minecraft/cornerstone/events/RendererEventHandler.java
@@ -22,7 +22,8 @@ public class RendererEventHandler {
     }
 
     private static void render(double partialTicks, EntityPlayer entityplayer) {
-        ArrayList<BlockPos> cornerstones = PlayerData.get(entityplayer.getUniqueID()).getCornerstoneUnderConstruction();
+        ArrayList<BlockPos> cornerstones = new ArrayList<>();
+        cornerstones.addAll(PlayerData.get(entityplayer.getUniqueID()).getCornerstoneUnderConstruction());
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferbuilder = tessellator.getBuffer();
         GlStateManager.disableTexture2D();
@@ -37,59 +38,53 @@ public class RendererEventHandler {
         bufferbuilder.begin(3, DefaultVertexFormats.POSITION_COLOR);
 
         int maxLen = Cornerstone.settings.getMaxPlotLength();
-        ArrayList<Double> pointsX = new ArrayList<>();
-        ArrayList<Double> pointsZ = new ArrayList<>();
+        ArrayList<Integer> pointsX = new ArrayList<>();
+        ArrayList<Integer> pointsZ = new ArrayList<>();
         for (BlockPos pos : cornerstones) {
-            double x = pos.getX();
-            double y = pos.getY();
-            double z = pos.getZ();
+            int x = pos.getX();
+            int y = pos.getY();
+            int z = pos.getZ();
             boolean drawX = true;
             boolean drawZ = true;
             for (BlockPos pos2 : cornerstones) {
                 if (pos == pos2) continue;
                 if (pos.getX() == pos2.getX()) {
                     drawZ = false;
-                    addLine(bufferbuilder, x + 0.5, y + 0.5, z + 0.5,
-                            x + 0.5, y + 0.5, pos2.getZ() + 0.5, dx, dy, dz);
+                    addLine(bufferbuilder, new int[]{x, y, z}, new int[]{x, y, pos2.getZ()}, dx, dy, dz);
                 }
                 if (pos.getZ() == pos2.getZ()) {
                     drawX = false;
-                    addLine(bufferbuilder, x + 0.5, y + 0.5, z + 0.5,
-                            pos2.getX() + 0.5, y + 0.5, z + 0.5, dx, dy, dz);
+                    addLine(bufferbuilder, new int[]{x, y, z}, new int[]{pos2.getX(), y, z}, dx, dy, dz);
                 }
             }
             if (cornerstones.size() >= 3) {
                 if (pointsX.contains(x)) {
-                    pointsX.remove(x);
+                    pointsX.removeIf(integer -> integer == x);
                 } else {
                     pointsX.add(x);
                 }
                 if (pointsZ.contains(z)) {
-                    pointsZ.remove(z);
+                    pointsZ.removeIf(integer -> integer == z);
                 } else {
                     pointsZ.add(z);
                 }
                 continue;
             }
             if (drawX)
-                addLine(bufferbuilder, x + 1.5 - maxLen, y + 0.5, z + 0.5,
-                        x - 0.5 + maxLen, y + 0.5, z + 0.5, dx, dy, dz);
+                addLine(bufferbuilder, new int[]{x - maxLen, y, z}, new int[]{x + maxLen, y, z}, dx, dy, dz);
             if (drawZ)
-                addLine(bufferbuilder, x + 0.5, y + 0.5, z + 1.5 - maxLen,
-                        x + 0.5, y + 0.5, z - 0.5 + maxLen, dx, dy, dz);
+                addLine(bufferbuilder, new int[]{x, y, z - maxLen}, new int[]{x, y, z + maxLen}, dx, dy, dz);
         }
         if (cornerstones.size() >= 3 && pointsX.size() == 1 && pointsZ.size() == 1) {
             for (BlockPos pos : cornerstones) {
-                double x = pos.getX();
-                double y = pos.getY();
-                double z = pos.getZ();
+                int x = pos.getX();
+                int y = pos.getY();
+                int z = pos.getZ();
                 if (pos.getX() == pointsX.get(0)) {
-                    addLine(bufferbuilder, x + 0.5, y + 0.5, z + 0.5,
-                            x + 0.5, y + 0.5, pointsZ.get(0) + 0.5, dx, dy, dz);
+                    addLine(bufferbuilder, new int[]{x, y, z}, new int[]{x, y, pointsZ.get(0)}, dx, dy, dz);
                 }
                 if (pos.getZ() == pointsZ.get(0)) {
-                    addLine(bufferbuilder, x + 0.5, y + 0.5, z + 0.5,
-                            pointsX.get(0) + 0.5, y + 0.5, z + 0.5, dx, dy, dz);
+                    addLine(bufferbuilder, new int[]{x, y, z}, new int[]{pointsX.get(0), y, z}, dx, dy, dz);
                 }
             }
         }
@@ -112,6 +107,24 @@ public class RendererEventHandler {
                 .color(0.25F, 0.25F, 1.0F, 1.0F).endVertex();
         bufferBuilder.pos(sx - dx, sy - dy, sz - dz)
                 .color(0.25F, 0.25F, 1.0F, 0.0F).endVertex();
+    }
+
+    private static void addLine(BufferBuilder bufferBuilder, int[] s, int[] e, double dx, double dy, double dz) {
+        if (s.length != 3 || e.length != 3) return;
+        int d = (s[0] == e[0]) ? 2 : (s[2] == e[2]) ? 0 : -1;
+        if (d == -1) return;
+        if (s[d] < e[d]) {
+            int tmp = e[d];
+            e[d] = s[d];
+            s[d] = tmp;
+        }
+        s[d]++;
+        for (int i = 0; i < 2; i++) {
+            addLine(bufferBuilder, s[0], s[1], s[2], e[0], e[1], e[2], dx, dy, dz);
+            addLine(bufferBuilder, s[0], s[1] + 1, s[2], e[0], e[1] + 1, e[2], dx, dy, dz);
+            s[(d == 0) ? 2 : 0]++;
+            e[(d == 0) ? 2 : 0]++;
+        }
     }
 
     @SubscribeEvent
